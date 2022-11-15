70f21dbcb3cbf92577e12340938632b3afcf582d
yeomeme <yeomeme@gmail.com>
2022-11-15 17:09:17 +0900

    리소스 추가

---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by yeomim Kim on 2022/11/15.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
---
+++ Africa/View/AnimalDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by yeomim Kim on 2022/11/15.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimalDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimalDetailView()
+    }
+}
