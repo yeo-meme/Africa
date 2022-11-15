5260d934d7292dd6cfee19c62febbec787595c80
yeomeme <yeomeme@gmail.com>
2022-11-15 16:43:45 +0900

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by yeomim Kim on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
