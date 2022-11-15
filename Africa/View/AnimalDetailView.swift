5ded418ae9f66d7da9b8d4f3fa4dc827e6eb46ba
yeomeme <yeomeme@gmail.com>
2022-11-15 17:38:18 +0900

    launch image - color set ->  프로젝트 다크모드 설정

--- Africa/View/AnimalDetailView.swift
+++ Africa/View/AnimalDetailView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct AnimalDetailView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
     }
 }
 
-struct AnimalDetailView_Previews: PreviewProvider {
+struct AnimalDetailView_Previews:
+    PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
     static var previews: some View {
-        AnimalDetailView()
+        NavigationView{
+            AnimalDetailView(animal: animals[0])
+            
+        }
     }
 }
---
+++ Africa/View/VideoListItemView.swift
@@ -0,0 +1,23 @@
+//
+//  VideoListItemView.swift
+//  Africa
+//
+//  Created by yeomim Kim on 2022/11/15.
+//
+
+import SwiftUI
+
+struct VideoListItemView: View {
+    
+    var body: some View {
+        HStack(spacing: 10) {
+            
+        }
+    }
+}
+
+struct VideoListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListItemView()
+    }
+}
